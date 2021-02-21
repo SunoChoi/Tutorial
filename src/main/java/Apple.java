f09cb022c7c928833b6954c74ee63d6f06637340
suchoi53@coupang.com <suchoi53@coupang.com>
2021-02-21 15:59:00 +0900

    Update test.pipeline

---
+++ src/main/java/Apple.java
@@ -0,0 +1,8 @@
+
+
+public class Apple{
+    public static final Apple FUJI = new Apple();
+    public static final Apple PIPPIN = new Apple();
+    public static final Apple GRANNY_SMITH = new Apple();
+    private Apple(){}
+}
